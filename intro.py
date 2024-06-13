898f8f43ec4360c1dc16ddb9c86a0e1fc306c2c3
Drake Bellessa <drakebellessa@gmail.com>
2024-06-13 09:35:29 -0400

    Added hand chart

--- intro.py
+++ intro.py
@@ -12,7 +12,13 @@ def main():
     title_font = pygame.font.SysFont("timesnewroman", 40)
     instruction_font = pygame.font.SysFont("timesnewroman", 26)
     title_caption = title_font.render("Poker: Texas Hold'em edition", True, pygame.Color("WHITE"))
-    instruction_caption = instruction_font.render("A to check, S to raise, D to call, F to fold.", True, pygame.Color("WHITE"))
+    instruction_caption1 = instruction_font.render("Texas Hold'em is played by finding various matches between your own two cards and the 5 cards in the center.", True, pygame.Color("WHITE"))
+    instruction_caption2 = instruction_font.render("A to check, S to raise, D to call, F to fold.", True, pygame.Color("WHITE"))
+
+    hands = pygame.image.load("poker_hands.jpg")
+    hands = pygame.transform.scale(hands, (440,320))
+    hands.set_colorkey((2,103,85))
+    #hands.set_colorkey((6,102,85))
 
     clock = pygame.time.Clock()
     while True:
@@ -28,7 +34,9 @@ def main():
         play_button.draw()
 
         screen.blit(title_caption, (445, screen.get_height()/5))
-        screen.blit(instruction_caption, (445, screen.get_height()/3))
+        screen.blit(instruction_caption1, (130, 230))
+        screen.blit(instruction_caption2, (465, 620))
+        screen.blit(hands, (460,280))
 
         pygame.display.update()
 main()
