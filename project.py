500d16f2f59ded6d034f3f28eff8967183608994
noahross <noahr688@gmail.com>
2024-06-13 10:18:32 -0400

    Chip counter added and testing change

--- project.py
+++ project.py
@@ -34,7 +34,11 @@ def main():
     ai0 = AI.AI(screen, 0)
     ai1 = AI.AI(screen, 1)
     ai2 = AI.AI(screen, 2)
-    players_chips = chip_counter.ChipCounter(screen, 500, 750)
+    players_chips = chip_counter.ChipCounter(screen, 455, 750)
+    ai_chips1 = chip_counter.ChipCounter(screen, 100,200)
+    ai_chips2 = chip_counter.ChipCounter(screen, 900,200)
+    ai_chips3 = chip_counter.ChipCounter(screen, 1300,750)
+
     winner = None
 
 
@@ -57,10 +61,20 @@ def main():
 
         screen.blit(grad_hat, (1000, 650))
         screen.blit(poker_chip, (400,600))
+        screen.blit(poker_chip, (45,65))
+        screen.blit(poker_chip, (845,65))
+        screen.blit(poker_chip, (1250,600))
+
+
+
         ai0.draw()
         ai1.draw()
         ai2.draw()
         players_chips.draw()
+        ai_chips1.draw()
+        ai_chips2.draw()
+        ai_chips3.draw()
+
 
         if game_round == 5:
             ai0.hand_check(cards)
